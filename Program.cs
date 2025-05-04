3731b9207cf7e7f84f05336dd32646dc16f0d1e7
MoIsmail <mhs.m.000@gmail.com>
2025-05-04 17:35:27 +0300

    move endpoints into controller

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<TodoContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
@@ -36,50 +37,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/todoitems", async (TodoContext db) =>
-    await db.TodoItems.ToListAsync());
-
-app.MapGet("/todoitems/complete", async (TodoContext db) =>
-    await db.TodoItems.Where(t => t.IsComplete).ToListAsync());
-
-app.MapGet("/todoitems/{id}", async (int id, TodoContext db) =>
-    await db.TodoItems.FindAsync(id)
-        is TodoItem todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
-
-app.MapPost("/todoitems", async (TodoItem todo, TodoContext db) =>
-{
-    db.TodoItems.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/todoitems/{todo.Id}", todo);
-});
-
-app.MapPut("/todoitems/{id}", async (int id, TodoItem inputTodo, TodoContext db) =>
-{
-    var todo = await db.TodoItems.FindAsync(id);
-
-    if (todo is null) return Results.NotFound();
-
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/todoitems/{id}", async (int id, TodoContext db) =>
-{
-    if (await db.TodoItems.FindAsync(id) is TodoItem todo)
-    {
-        db.TodoItems.Remove(todo);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-
-    return Results.NotFound();
-});
 
 app.Run();
