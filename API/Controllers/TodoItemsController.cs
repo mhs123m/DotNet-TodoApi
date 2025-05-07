a115ec1053f41825b32f5bc4c1d18eed59839589
MoIsmail <mhs.m.000@gmail.com>
2025-05-07 12:52:23 +0300

    now it's finished

--- API/Controllers/TodoItemsController.cs
+++ API/Controllers/TodoItemsController.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
+using Infrastructure.Persistence;
+using Domain.Entities;
 
 namespace TodoApi.Controllers
 {
